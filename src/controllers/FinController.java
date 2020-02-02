1e552d7644a35467ce202287b2d918b3645e0b13
NicolArrayList <NicolArrayList@users.noreply.github.com>
2020-02-02 13:05:23 +0100

    Miaou patron

--- src/controllers/FinController.java
+++ src/controllers/FinController.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 public class FinController {
     public ImageView eau;
     private Timeline tl;
-    private int index_tl;
+    private int index_tl = 0;
     private static ArrayList<Image> img_eau = new ArrayList<>(Arrays.asList(new Image("images/waterfall/W1001.png"),new Image("images/waterfall/W1002.png"),new Image("images/waterfall/W1003.png")
             ,new Image("images/waterfall/W1004.png")
             ,new Image("images/waterfall/W1005.png")
@@ -21,7 +21,16 @@ public class FinController {
             ,new Image("images/waterfall/W1007.png")
             ,new Image("images/waterfall/W1008.png")
     ));
-
+    public ImageView perso;
+    private Timeline tl_perso;
+    private int index_tl_perso = 0;
+    private static ArrayList<Image> img_perso = new ArrayList<>(Arrays.asList(new Image("images/waterfall/W1001.png"),new Image("images/waterfall/W1002.png"),new Image("images/waterfall/W1003.png")
+            ,new Image("images/waterfall/W1004.png")
+            ,new Image("images/waterfall/W1005.png")
+            ,new Image("images/waterfall/W1006.png")
+            ,new Image("images/waterfall/W1007.png")
+            ,new Image("images/waterfall/W1008.png")
+    ));
     public void initialize() {
         tl = new Timeline();
         tl.setCycleCount(Animation.INDEFINITE);
@@ -29,5 +38,12 @@ public class FinController {
             eau.setImage(img_eau.get((index_tl++) % img_eau.size()));
         }));
         tl.play();
+
+        tl_perso = new Timeline();
+        tl_perso.setCycleCount(Animation.INDEFINITE);
+        tl_perso.getKeyFrames().add(0, new KeyFrame(Duration.millis(50), e -> {
+            perso.setImage(img_perso.get((index_tl_perso++) % img_perso.size()));
+        }));
+        tl_perso.play();
     }
 }
