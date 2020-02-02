f5ae53df15c2d376610e857c09ad2308df0af007
TheOrtech <aurelienchevallereau@icloud.com>
2020-02-02 15:07:15 +0100

    Ajout page règles

--- src/controllers/ReglesController.java
+++ src/controllers/ReglesController.java
@@ -1,7 +1,18 @@
 package controllers;
 
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import mains.Main;
+import mains.SceneLoader;
+
+import java.io.IOException;
 
 public class ReglesController {
 
+    public Button BoutonFermer;
+
+    public void RetourMenu(MouseEvent mouseEvent) throws IOException {
+        Main.sceneLoader.switchTo(SceneLoader.SCENE_HOME);
+    }
 }
